5bc45ed9219f52de54feee41c9fa16871a663e7b
therealhomeslice <ron.puertollano@gmail.com>
2017-10-19 05:25:15 +0000

    Updated Taxes

--- 01_taxes/tax_collector.py
+++ 01_taxes/tax_collector.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import sys
 
 # Taxes script w/o deductions 
 
@@ -23,11 +22,32 @@ def calculateSingle(s):
       taxes += tax_bracket[x] * tax_rate[x-1]
   return taxes
 
-if __name__ == '__main__':
+def calculateNJState(s):
+  tax_bracket = [0, 20001, 35001, 40001, 75001, 500001]
+  tax_rate = [.014, .0175, .035, .05525, .0637, .0897]
 
-#  test_cases = [1000, 9000, 10000, 50000, 100000, 400000, 500000]
-#  for x in range(len(test_cases)):
-#    print "Test Case " + str(test_cases[x]) + ": " + str(calculateSingle(test_cases[x]))
-  print calculateSingle(int(sys.argv[1]))
+  bracket = 0
+  remainder = 0
+  taxes = 0
+  if s >= 415051:
+    return (s-415051)*.396+120528.5 #hardcoded taxes
+  for x in range(len(tax_bracket)-1):
+    if s > tax_bracket[x] and s < tax_bracket[x+1]:
+      bracket = x
+      remainder = s - tax_bracket[x] 
+      taxes += remainder*tax_rate[x]
+  for x in range(0, bracket+1):
+    if x > 1:
+      taxes += (tax_bracket[x] - tax_bracket[x-1])*tax_rate[x-1]
+    else:
+      taxes += tax_bracket[x] * tax_rate[x-1]
+  return taxes
 
+def socialSecurityTax(s):
+  return s*.062
 
+def medicareTax(s):
+  return s*.0145
+
+#if __name__ == '__main__':
+  
